37d75c5ed72406c66c1e6a31c2d6706b242133d5
Nathan <n.auckett@gmail.com>
2022-10-30 22:14:09 +1100

    Did some cleaning with components. Made Call and email icons functional

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -12,6 +12,36 @@ import CallIcon from "../../icons/call.svg"
 import EmailIcon from "../../icons/mail.svg"
 import AddressIcon from "../../icons/home.svg"
 
+function InfoListItem(props){
+    return (
+        <ListGroup.Item>
+            <Container>
+                <Row>
+                    <Col xs={2}>
+                        <a href={props.url} target="_blank">
+                            <img src = {props.icon} width="32px"></img>
+                        </a>
+                    </Col>
+                    <Col className='text-end align-self-center'>{props.info}</Col>
+                </Row>
+            </Container>
+        </ListGroup.Item>
+    )
+}
+
+function AddressInfoListItem(props){
+    return (
+        <ListGroup.Item>
+            <Container>
+                <Row>
+                    <Col xs={4}>{props.label}</Col>
+                    <Col className='text-end'>{props.info}</Col>
+                </Row>
+            </Container>
+        </ListGroup.Item>
+    )
+}
+
 export default function Contact(props){
     const contact = props.contact;
 
@@ -20,22 +50,8 @@ export default function Contact(props){
         <Card className='contactCard bg-secondary text-light'>
             <Card.Header className='text-center'>{contact.name}</Card.Header>
             <ListGroup variant="flush">
-                <ListGroup.Item>
-                    <Container>
-                        <Row>
-                            <Col xs={2}><img src = {CallIcon} width="32px"></img></Col>
-                            <Col className='text-end'>{contact.phone}</Col>
-                        </Row>
-                    </Container>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Container>
-                        <Row>
-                            <Col xs={2}><img src = {EmailIcon} width="32px"></img></Col>
-                            <Col className='text-end'>{contact.email}</Col>
-                        </Row>
-                    </Container>
-                </ListGroup.Item>
+                <InfoListItem icon={CallIcon} info={contact.phone} url={`tel: ${contact.phone}`}/>
+                <InfoListItem icon={EmailIcon} info={contact.email} url={`mailto: ${contact.email}`}/>
                 <ListGroup.Item >
                     <Accordion flush>
                         <Accordion.Header>
@@ -48,30 +64,9 @@ export default function Contact(props){
                         </Accordion.Header>
                         <Accordion.Body>
                             <ListGroup>
-                                <ListGroup.Item>
-                                    <Container>
-                                        <Row>
-                                            <Col xs={4}>Address:</Col>
-                                            <Col className='text-end'>{contact.address.suite}, {contact.address.street}</Col>
-                                        </Row>
-                                    </Container>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Container>
-                                        <Row>
-                                            <Col>City:</Col>
-                                            <Col className='text-end'>{contact.address.city}</Col>
-                                        </Row>
-                                    </Container>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Container>
-                                        <Row>
-                                            <Col>Zip/Post Code:</Col>
-                                            <Col className='text-end'>{contact.address.zipcode}</Col>
-                                        </Row>
-                                    </Container>
-                                </ListGroup.Item>
+                                <AddressInfoListItem label="Address:" info={`${contact.address.suite}, ${contact.address.street}`}/>
+                                <AddressInfoListItem label="City:" info={contact.address.city}/>
+                                <AddressInfoListItem label="Zip/Post Code:" info={contact.address.zipcode}/>
                             </ListGroup>
                         </Accordion.Body>
                     </Accordion>
@@ -80,22 +75,5 @@ export default function Contact(props){
             </ListGroup>
         </Card>
         </Col>
-        // <Container className = "contactCard">
-        //     <Row>
-        //         <Col>Name:</Col>
-        //         <Col>{contact.name}</Col>
-        //     </Row>
-        //     <Row>
-        //         <Col>Phone:</Col>
-        //         <Col>{contact.phone}</Col>
-        //     </Row>
-        //     <Row>
-        //         <Col>Email:</Col>
-        //         <Col>{contact.email}</Col>
-        //     </Row>
-        //     <Row>
-        //         <Col>Address:</Col>
-        //     </Row>
-        // </Container>
     )
 }
